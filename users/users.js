2908a0916fccd0666069a32cc213c058be6a75e3
Suffyan Tariq <suffyan.tariq@gmail.com>
2020-03-27 09:33:49 -0700

    added users-model

---
+++ users/users.js
@@ -0,0 +1,37 @@
+const db = require('../database/dbConfig')
+
+module.exports ={
+    add,
+    find,
+    findBy,
+    findByID,
+    remove
+};
+
+function find(){
+    return db('users').select('id', 'username', 'password');
+}
+
+function findBy(filter){
+    return db('users').where(filter);
+}
+
+function add(user){
+    return db('users')
+    .insert(user, 'id')
+    .then(ids => {
+        const [id] = ids;
+        return findById(id);
+    });
+}
+
+function findById(id){
+    return db('users')
+    .where({ id })
+    .first();
+}
+
+function remove(id){
+    return db('users')
+    .del(id)
+}
