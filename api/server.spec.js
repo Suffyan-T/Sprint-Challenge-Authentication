1bec310210b78b6b6ad5c20da23da7b90c9ce7d4
Suffyan Tariq <suffyan.tariq@gmail.com>
2020-03-27 10:40:08 -0700

    fixed error in test

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 const server = require('./server');
-const db = require('../auth/user-model');
+const db = require('../users/users');
 const knexDB = require('../database/dbConfig')
-const authenticate = require('../auth/authenticate-middleware');
 const request = require('supertest');
 
 describe('GET/', () => {
@@ -25,12 +24,11 @@ describe('POST/', () => {
         await knexDB('users').truncate();
     })
     //TEST 
-    it('should insert 2 new users}', async () => {
+    it('should insert a user}', async () => {
         await db.add({username:'User1', password: 'password1'});
-        await db.add({username: 'User2', password: 'password2'});
 
         const users = await knexDB('users');
-        expect(users).toHaveLength(2);
+        expect(users).toHaveLength(1);
     })
     it('should return 200 ok', async () => {
         const res = await request(server).get('/');
